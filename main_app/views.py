aaeb976271a0617b8ed1dcdf843dbc1196142610
Ripley Mayfield <ripleymay@Ripleys-MBP.attlocal.net>
2021-11-11 15:05:22 -0600

    adds auth and more css

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.db.models import Sum
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from .models import Category, Activity, Log
 from .forms import LogForm
@@ -17,6 +19,7 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def dashboard(request):
   key = os.environ['ZEN_API_KEY']
   quote = requests.get(f'https://zenquotes.io/api/today/{key}').json()
@@ -33,42 +36,63 @@ def dashboard(request):
     title = list(filter(lambda cat: cat.id == ag['activity__categories'], categories))[0].title
     totals.append({'title': title, 'mins': ag['mins']})
 
-  return render(request, 'categories/index.html', { 'categories': categories, 'totals': totals, 'html': quote[0]['h'] })  
+  return render(request, 'categories/index.html', { 
+    'categories': categories, 
+    'totals': totals, 
+    'html': quote[0]['h'] 
+  })  
 
-class ActivityCreate(CreateView):
+class ActivityCreate(LoginRequiredMixin, CreateView):
   model = Activity
   fields = ['title', 'description', 'categories']
   # will be called if the activity data is valid
   def form_valid(self, form):
-    # form.instance is the in-memory new activity obj
     form.instance.user = self.request.user
-    # Let the CreateView's form_valid method do its job
     return super().form_valid(form)
 
-class ActivityUpdate(UpdateView):
+class ActivityUpdate(LoginRequiredMixin, UpdateView):
   model = Activity
   fields = ['title', 'description', 'categories']
 
-class ActivityDelete(DeleteView):
+class ActivityDelete(LoginRequiredMixin, DeleteView):
   model = Activity
   success_url = '/dashboard/'
 
+@login_required
 def activity_list(request):
+  categories = Category.objects.all()
   category = request.GET.get('category')
   if category:
     activities = Activity.objects.filter(user=request.user, categories__title=category)
   else:
     activities = Activity.objects.filter(user=request.user)
-  return render(request, 'activities/index.html', { 'activities': activities, 'category': category })
+  return render(request, 'activities/index.html', { 
+    'categories': categories,
+    'activities': activities, 
+    'category': category
+  })
 
+@login_required
 def activity_detail(request, activity_id):
   activity = Activity.objects.get(id=activity_id)
   log_form = LogForm()
+
+  today = datetime.date.today()
+  week_ago = today - datetime.timedelta(days=6)
+  month_ago = today - datetime.timedelta(days=29)
+  last_week_logs = Log.objects.filter(activity=activity, date_completed__range=(week_ago, today))
+  last_month_logs = Log.objects.filter(activity=activity, date_completed__range=(month_ago, week_ago))
+  all_prior_logs = Log.objects.filter(activity=activity, date_completed__lt=(month_ago))
+
   return render(request, 'activities/detail.html', {
     'activity': activity,
+    'last_week_logs': last_week_logs,
+    'last_month_logs': last_month_logs,
+    'all_prior_logs': all_prior_logs,
     'log_form': log_form,
   })
 
+@login_required
 def add_log(request, activity_id):
   # create a ModelForm instance using the data in the posted form
   form = LogForm(request.POST)
@@ -79,6 +103,7 @@ def add_log(request, activity_id):
     new_log.save()
   return redirect('activity_detail', activity_id=activity_id)
 
+@login_required
 def delete_log(request, activity_id, log_id):
   Log.objects.filter(id=log_id).delete()
   return redirect('activity_detail', activity_id=activity_id)
@@ -87,12 +112,9 @@ def signup(request):
   error_message = ''
   if request.method == 'POST':
     # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('home')
     else:
